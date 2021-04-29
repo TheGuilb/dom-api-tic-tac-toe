6710ff17b7012cacbab00f7d3c8d3331ea31d771
Taylor <tw10262@gmail.com>
2021-04-29 15:10:03 -0700

    almost there boi

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,24 +1,83 @@
 window.addEventListener("DOMContentLoaded", event => {
     const board = document.getElementById("tic-tac-toe-board");
     const square = document.querySelectorAll(".square");
+    const header = document.getElementById("game-status")
+    header.innerText = "";
+    
+    let arr = Array(9)
+    
+    let gameStatus = (arr) => {
+        if(arr[0] === arr[1] && arr[1] === arr[2] && arr[2] !== ""){
+            
+        }
+    }
+    
+    
+    
 
+
+
+
+console.log(arr)
     clickCounter = 0;
     board.addEventListener("click", event => {
-        clickCounter++;
-            if
-            (event.target.innerText.length !== 0 ) {
-                event.preventDefault();
-                clickCounter--;
-            } else if (clickCounter % 2 !== 0) {
-                var player1 = "X";
-                event.target.innerText = player1;
-                console.log(event.target);
-                console.log(player1);
+        if(!event.target.id.includes("square-")) return; 
+        let squrNum = event.target.id.split("-");
+        
+        let [square,number] = squrNum
+        //console.log(arr[number])
+            //console.log(squrNum)
+            arr.push()
+            if(arr[number]) return;
+            clickCounter++;
+            
+            if (clickCounter % 2 !== 0) {
+                let playerXtoken = "X"
+                const X = new Image(115);
+                X.src = "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg"
+                const playerX = X;
+                event.target.appendChild(playerX)
+                arr[number] = playerXtoken
+                var playerXmoves = clickCounter / 2 + .5; 
+                
+                //arr.push(square[number])
+                //console.log(arr)
+                //console.log(playerXmoves)
+                
             } else {
-                var player2 = "O"
-                event.target.innerText = player2;
+                let playerOtoken = "O"
+                const O = new Image(120);
+                O.src = "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg"
+                const playerO = O
+                event.target.appendChild(playerO)
+                arr[number] = playerOtoken
+                var playerOmoves = clickCounter / 2;
+            
             }
 
-        console.log(clickCounter);
-    });
+        
+        console.log(arr)
+        if(arr.length >= 5 ){
+            
+        
+            arr.forEach((ele, index) => {
+                let one = arr[index]
+                let two = arr[index +1];
+                let three = arr[index +2]; 
+                console.log(one)
+                console.log(two)
+                //if(){}
+                
+            })
+        
+        }
+    
+    
+    
+        });
+
+        
+
+    
+
 });
