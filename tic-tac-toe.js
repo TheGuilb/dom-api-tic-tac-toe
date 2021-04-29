768ad78019d59da075455b3fb91eab3cee8b64bd
Pierre Guilbault <guilbaultpj@gmail.com>
2021-04-29 18:51:21 -0400

    Getting there, we can do this!

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,36 +1,64 @@
 window.addEventListener("DOMContentLoaded", event => {
     const board = document.getElementById("tic-tac-toe-board");
     const square = document.querySelectorAll(".square");
-    const header = document.getElementById("game-status")
+    let header = document.getElementById("game-status")
     header.innerText = "";
-    
-    let arr = Array(9)
-    
-    let gameStatus = (arr) => {
+    // if (header !== "") {
+    //     event.preventDefault();
+    // }
+    const arr = Array(9).fill("");
+    let gameStatus = () => {
+        console.log(arr)
         if(arr[0] === arr[1] && arr[1] === arr[2] && arr[2] !== ""){
-            
+            var winner = arr[0]
+            header.innerText = `${winner} is the winner!`
         }
+        if(arr[3] === arr[4] && arr[4] === arr[5] && arr[5] !== ""){
+            var winner = arr[3]
+            header.innerText = `${winner} is the winner!`
+        }
+        if(arr[6] === arr[7] && arr[7] === arr[8] && arr[8] !== ""){
+            var winner = arr[6]
+            header.innerText = `${winner} is the winner!`
+        }
+        if(arr[0] === arr[3] && arr[3] === arr[6] && arr[6] !== ""){
+            var winner = arr[0]
+            header.innerText = `${winner} is the winner!`
+        }
+        if(arr[1] === arr[4] && arr[4] === arr[7] && arr[7] !== ""){
+            var winner = arr[1]
+            header.innerText = `${winner} is the winner!`
+        }
+        if(arr[2] === arr[5] && arr[5] === arr[8] && arr[8] !== ""){
+            var winner = arr[2]
+            header.innerText = `${winner} is the winner!`
+        }
+        if(arr[0] === arr[4] && arr[4] === arr[8] && arr[8] !== ""){
+            var winner = arr[0]
+            header.innerText = `${winner} is the winner!`
+        }
+        if(arr[2] === arr[4] && arr[4] === arr[6] && arr[6] !== ""){
+            var winner = arr[2]
+            header.innerText = `${winner} is the winner!`
+        }
+        // if (!arr.includes("")) {
+        //     header.innerText = "It's a TIE -fighter"
+        // }
     }
-    
-    
-    
 
-
-
-
-console.log(arr)
+// console.log(arr)
     clickCounter = 0;
     board.addEventListener("click", event => {
-        if(!event.target.id.includes("square-")) return; 
+        if(!event.target.id.includes("square-")) return;
         let squrNum = event.target.id.split("-");
-        
+
         let [square,number] = squrNum
         //console.log(arr[number])
             //console.log(squrNum)
-            arr.push()
+            // arr.push()
             if(arr[number]) return;
             clickCounter++;
-            
+
             if (clickCounter % 2 !== 0) {
                 let playerXtoken = "X"
                 const X = new Image(115);
@@ -38,12 +66,12 @@ console.log(arr)
                 const playerX = X;
                 event.target.appendChild(playerX)
                 arr[number] = playerXtoken
-                var playerXmoves = clickCounter / 2 + .5; 
-                
+                var playerXmoves = clickCounter / 2 + .5;
+
                 //arr.push(square[number])
                 //console.log(arr)
                 //console.log(playerXmoves)
-                
+
             } else {
                 let playerOtoken = "O"
                 const O = new Image(120);
@@ -52,32 +80,14 @@ console.log(arr)
                 event.target.appendChild(playerO)
                 arr[number] = playerOtoken
                 var playerOmoves = clickCounter / 2;
-            
+
             }
 
-        
+
         console.log(arr)
-        if(arr.length >= 5 ){
-            
-        
-            arr.forEach((ele, index) => {
-                let one = arr[index]
-                let two = arr[index +1];
-                let three = arr[index +2]; 
-                console.log(one)
-                console.log(two)
-                //if(){}
-                
-            })
-        
-        }
-    
-    
-    
+            if(clickCounter >= 5 ){
+                gameStatus();
+
+            }
         });
-
-        
-
-    
-
 });
